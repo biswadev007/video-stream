3cbc2616b0cd56b697864f161f0db95c1e90d1ef
Biswajit Paul <biswa6328@gmail.com>
2024-05-13 11:29:34 +0530

    Add css to video player

--- frontend/src/VideoPlayer.jsx
+++ frontend/src/VideoPlayer.jsx
@@ -5,13 +5,13 @@ import 'video.js/dist/video-js.css';
 const VideoPlayer = (props) => {
   const videoRef = React.useRef(null);
   const playerRef = React.useRef(null);
-  const {options, onReady} = props;
+  const { options, onReady } = props;
 
   React.useEffect(() => {
 
     // Make sure Video.js player is only initialized once
     if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
       const videoElement = document.createElement("video-js");
 
       videoElement.classList.add('vjs-big-play-centered');
@@ -22,8 +22,8 @@ const VideoPlayer = (props) => {
         onReady && onReady(player);
       });
 
-    // You could update an existing player in the `else` block here
-    // on prop change, for example:
+      // You could update an existing player in the `else` block here
+      // on prop change, for example:
     } else {
       const player = playerRef.current;
 
@@ -46,7 +46,7 @@ const VideoPlayer = (props) => {
 
   return (
     <div data-vjs-player>
-      <div ref={videoRef} />
+      <div className='video-play' ref={videoRef} />
     </div>
   );
 }
